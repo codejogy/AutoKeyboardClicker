8cc9d443c86f8eff290af0a53ffd4eeb8408bb64
Jose <josehdzibarra1@gmail.com>
2023-08-09 22:14:14 -0600

    Added comments and keyBuffer is a feature TODO to register hotkey

--- Controller.py
+++ Controller.py
@@ -33,18 +33,27 @@ class Initialize:
         self.botonEliminar.configure(command=self.deleteItem)
         self.listBoton = self.loadData()
 
-        #Crear binds
+        # Crear binds
         # self.treeCreador.bind("<Double-1>",lambda e:self.checkEvents(e))
         self.treeCreador.bind("<1>",lambda e:self.checkEventsTree(e))
 
+        # Variables
         self.num = self.valID() # Para los valores por defecto
         self.time = time.time()
         self.boolTimer = False
         self.segundoClick = time.time()
         self.primerClick = time.time()
+        
+        #Creado exclusivamente para el método keyBuffer
+        self.buffTimeFirst = time.time()
+        self.buffTimeSecond = 0
+        self.boolbuffer = False
+        
+        # -----------COMENZAR-----------
     def start(self):
         self.ventana.mainloop()
 
+        # -----------MÉTODO PARA agregar-----------
     def valID(self):
         if self.listBoton == {}:
             return 0
@@ -52,7 +61,7 @@ class Initialize:
         x = int(str(x)[1:]) # Obtener el número en forma de int del último ID
         return x
 
-
+        # -----------CALLBACK PARA BOTÓN agregar -----------
     def agregar(self):
 
         id = self.treeCreador.insert("","end",text=f"default{self.num}",values=[f"prueba{self.num}"])
@@ -61,6 +70,8 @@ class Initialize:
         # print(self.listBoton)
         print(self.listBoton)
         self.num +=1
+        
+        # ----------- SALVAR DATOS AL SALIR -----------
     def saveData(self,data):
         with open(PATHJS,"w") as file:
             json.dump(data,file,indent=2) # El indent permite visual appealing
@@ -68,7 +79,8 @@ class Initialize:
 
     def updateItem(self):
         pass
-
+        
+        #-----------CALLBACK PARA EL DOBLE CLICK-----------
     def checkEventsTree(self,event):
         interval = 0.2# Para el doble click
 
@@ -112,29 +124,55 @@ class Initialize:
                 self.entryName.bind("<FocusOut>",lambda e: e.widget.place_forget())
                 self.entryName.bind("<Return>",enter)
 
-            if region == "cell": # Crear hotkey
+            elif region == "cell": # Crear hotkey
                 item = self.treeCreador.focus()
                 values = self.treeCreador.item(item)
                 bbox = self.treeCreador.bbox(item,column)
-                self.textHotkey.place(x=bbox[0],y=bbox[1]+21,w=bbox[2],h=bbox[3])
+                self.textHotkey.place(x=bbox[0],y=(bbox[1]+21),w=bbox[2],h=bbox[3])
                 self.textHotkey.focus()
                 #Identificar teclas
-                self.textHotkey.bind("<Key>",lambda e: print(e))
+                self.textHotkey.bind("<Key>",self.keyBuffer)
                 self.textHotkey.bind("<FocusOut>", lambda e: e.widget.place_forget())
                 self.textHotkey.bind("<FocusIn>",lambda e: print(e))
                 self.textHotkey.bind("<Return>", lambda e: e.widget.place_forget())
                 
             print(int(column[1:]))
-
-
-
-
-
+        
+        #-----------CAMBIAR EL VALOR DE BOOL UNA VEZ TERMINADA LA FUNCIÓN-----------
+        
         self.boolTimer = not self.boolTimer
         print(self.boolTimer)
         print(event)
 
-
+    #-----------CALLBACK QUE GUARDA LOS VALORES DEL HOTKEY DEL MOMENTO-----------
+    def keyBuffer(self,e):
+        # El hotkey se registrará con poco tiempo de intervalo dado que se haya presionado
+        # El buffer durará 3 segundos, despues de los cuales se borrará el hotkey
+        if not self.boolbuffer:
+            self.buffTimeFirst = e.time
+            
+        if self.boolbuffer:
+            self.buffTimeSecond = e.time
+            
+        deltaT = abs(self.buffTimeFirst-self.buffTimeSecond)
+            
+        print("Keysym:",e.keysym)
+        print("State:",e.state)
+        print("Char:",e.char)
+        print("KeyCode:",e.keycode)
+        print("Serial:",e.serial)
+        print("Time:",e.time,"ms")
+        print("Type:",e.type)
+        print("DeltaTime:",deltaT,"ms")
+        print(e)
+        print("-----------")
+        
+        if deltaT > 3000: #Mayor a 3 segundos
+            pass
+        else: # Menor a 3 segundos
+            pass
+        
+        self.boolbuffer = not self.boolbuffer # Se ira cambiando
 
 
     def loadData(self):
