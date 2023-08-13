d18889f97714f3047452dfe6585da67d642a55bb
Jose <josehdzibarra1@gmail.com>
2023-08-13 14:38:59 -0600

    Primer commit de model

--- Model.py
+++ Model.py
@@ -2,32 +2,35 @@
 # No logic
 # La data será
 
-#Composition
+# Composition
 import pynput
 
-class CombinacionTeclas():
+
+class CombinacionTeclas(pynput):
     """
     Clase con la lógica del programa para crear objetos
     que permita hacer que con una combinación de teclas
     se haga algo determinado
     """
+
     def __init__(self):
         """
         Se creará un entorno para una nueva combinación
         """
+        super().__init__()
+
         self.oMouse = pynput.mouse.Controller()
         self.oKeyboard = pynput.keyboard.Controller()
         self.LEFTCLICK = pynput.mouse.Button.left
         self.RIGHTCLICK = pynput.mouse.Button.right
         self.hotkey = ""
 
-
     def getKeyValue(self):
         pass
 
-    def combinacion(self,key:str) -> object:
+    def combinacion(self, key: str) -> object:
         # Verificar que sea string
-        assert key == str,"Este valor necesita ser un string"
+        assert key == str, "Este valor necesita ser un string"
         # Verificar que el string sea compatible
         try:
             key == pynput.keyboard.KeyCode()
@@ -40,9 +43,7 @@ class CombinacionTeclas():
         if key == pynput.keyboard.Key():
             pass
 
-
-
-    def listener(self,callback) :
+    def start(self, callback):
         self.listen = pynput.keyboard.Listener(on_release=callback)
         self.listen.start()
 
@@ -52,7 +53,7 @@ class CombinacionTeclas():
     def __repr__(self):
         pass
 
-if __name__ == '__main__':
-    print(pynput.keyboard.KeyCode(char="5").from_char(["7","5"]))
 
-
+if __name__ == "__main__":
+    oCombinacion = CombinacionTeclas()
+    oCombinacion.start()
